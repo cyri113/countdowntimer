30bcbb0813a19a86372a4f2d1d18ec48d6928577
Cyrille <c.derche@me.com>
2020-05-12 16:38:43 +0200

    xvfb directly

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,10 +4,10 @@ require "webdrivers/geckodriver"
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
+    driven_by :selenium
   end
 
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_headless
-  end
+  # config.before(:each, type: :system, js: true) do
+  #   driven_by :selenium_headless
+  # end
 end
