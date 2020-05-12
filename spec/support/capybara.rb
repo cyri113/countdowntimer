5681eb03e92d12261a1ad44bfffa15361565f8dc
Cyrille <c.derche@me.com>
2020-05-12 14:25:48 +0200

    custom driver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,13 @@
 require "capybara/rspec"
 
+Capybara.register_driver :selenium_chrome_headless do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.add_argument("headless")
+  browser_options.add_argument("disable-gpu")
+  browser_options.add_argument("no-sandbox")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
@@ -7,7 +15,7 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system, js: true) do
     # driven_by :selenium_chrome_headless
-    driven_by :selenium_chrome_headless, options: { args: ["disable-gpu", "no-sandbox"] }
+    driven_by :selenium_chrome_headless
     # https://dev.to/aergonaut/running-rails-5-system-tests-on-travis-ci-with-chromedriver-4nm7
     # driven_by :selenium, using: :chrome, options: { args: ["--headless", "--disable-gpu"] }
   end
