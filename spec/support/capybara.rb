bb520b33f7b131765ec4b5c43af800275b5c5710
Cyrille <c.derche@me.com>
2020-05-12 16:56:57 +0200

    trying new things

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,13 @@
 require "capybara/rspec"
-require "webdrivers/geckodriver"
-# require "webdrivers/chromedriver"
+# require "webdrivers/geckodriver"
+require "webdrivers/chromedriver"
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium
+    driven_by :rack_test
   end
 
-  # config.before(:each, type: :system, js: true) do
-  #   driven_by :selenium_headless
-  # end
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless, options: { args: ["disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+  end
 end
