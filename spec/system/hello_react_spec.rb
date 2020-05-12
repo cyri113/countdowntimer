9d029ad70ff916dd5362a004fb7523987202aea9
Cyrille <c.derche@me.com>
2020-05-12 16:32:33 +0200

    safe visit

--- spec/system/hello_react_spec.rb
+++ spec/system/hello_react_spec.rb
@@ -2,7 +2,27 @@ require "rails_helper"
 
 RSpec.describe "Hello React", type: :system, js: true do
   it "render text 'Hello React!'" do
-    visit events_url
+    safe_visit events_url
     expect(page).to have_text("Hello React!")
+  end
+end
+
+private
+
+def safe_visit(url)
+  max_retries = 3
+  times_retried = 0
+  begin
+    visit url
+  rescue Net::ReadTimeout => error
+    if times_retried < max_retries
+      times_retried += 1
+      puts "Failed to visit #{current_url}, retry #{times_retried}/#{max_retries}"
+      retry
+    else
+      puts error.message
+      puts error.backtrace.inspect
+      exit(1)
+    end
   end
 end
