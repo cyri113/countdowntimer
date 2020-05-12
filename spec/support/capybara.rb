1e195604fb16378ec5e07340a6ddbf513052d946
Cyrille <c.derche@me.com>
2020-05-12 15:28:30 +0200

    https://docs.travis-ci.com/user/chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,13 @@
 require "capybara/rspec"
 require "webdrivers/chromedriver"
 
-Capybara.register_driver :selenium_chrome_headless do |app|
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.add_argument("headless")
-  browser_options.add_argument("disable-gpu")
-  browser_options.add_argument("no-sandbox")
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+require "capybara"
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
+
+# Capybara.javascript_driver = :chrome
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
@@ -15,9 +15,13 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    # driven_by :selenium_chrome_headless
-    driven_by :selenium_chrome_headless
-    # https://dev.to/aergonaut/running-rails-5-system-tests-on-travis-ci-with-chromedriver-4nm7
-    # driven_by :selenium, using: :chrome, options: { args: ["--headless", "--disable-gpu"] }
+    driven_by :chrome
   end
+
+  # config.before(:each, type: :system, js: true) do
+  #   # driven_by :selenium_chrome_headless
+  #   driven_by :selenium_chrome_headless
+  #   # https://dev.to/aergonaut/running-rails-5-system-tests-on-travis-ci-with-chromedriver-4nm7
+  #   # driven_by :selenium, using: :chrome, options: { args: ["--headless", "--disable-gpu"] }
+  # end
 end
